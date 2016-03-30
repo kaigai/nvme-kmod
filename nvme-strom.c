02b508cc545a2510846ce63ac557408372b63a93
KaiGai Kohei <kaigai@kaigai.gr.jp>
2016-03-30 22:51:33 +0900

    a workaround

--- nvme-strom.c
+++ nvme-strom.c
@@ -7,14 +7,61 @@
  *
  *
  */
+#include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 
+
+/* entry of /proc/nvme-strom */
+static struct proc_dir_entry  *nvme_strom_proc = NULL;
+
+
+
+
+
+static int
+nvme_strom_proc_open(struct inode *inode, struct file *file)
+{
+	return -EINVAL;
+}
+
+static int
+nvme_strom_proc_release(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static long
+nvme_strom_proc_ioctl(struct file *file,
+					  unsigned int cmd,
+					  unsigned long arg)
+{
+	return -EINVAL;
+}
+
+
+
+
+/* device file operations */
+static const struct file_operations nvme_strom_fops = {
+	.owner			= THIS_MODULE,
+	.open			= nvme_strom_proc_open,
+	.release		= nvme_strom_proc_release,
+	.unlocked_ioctl	= nvme_strom_proc_ioctl,
+	.compat_ioctl	= nvme_strom_proc_ioctl,
+};
 
 static int	__init
 nvme_strom_init(void)
 {
-	printk(KERN_INFO "nvme_strom_init is called\n");
+	nvme_strom_proc = proc_create("nvme-strom",
+								  0444,
+								  NULL,
+								  &nvme_strom_fops);
+	if (!nvme_strom_proc)
+		return -ENOMEM;
+	printk(KERN_INFO "/proc/nvme-strom registered\n");
 
 	return 0;
 }
@@ -23,7 +70,8 @@ module_init(nvme_strom_init);
 static void __exit
 nvme_strom_exit(void)
 {
-	printk(KERN_INFO "nvme_strom_exit is called\n");
+	proc_remove(nvme_strom_proc);
+	printk(KERN_INFO "/proc/nvme-strom unregistered\n");
 }
 module_exit(nvme_strom_exit);
 
